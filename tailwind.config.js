32d2b72cc2ce5b08a00fb16673ce30429a865a6d
mihir-nath-2024 <mihirnath114@gmail.com>
2024-08-19 02:25:08 +0600

    meet section half done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient-180': 'linear-gradient(180deg, rgb(244, 244, 244), rgba(244, 244, 244, 0) 100%)',
+        'custom-gradient-270': 'linear-gradient(270deg, rgb(255, 0, 0) 0%, rgb(255, 137, 56) 100%)',
+      },
+    },
   },
   plugins: [],
 }
